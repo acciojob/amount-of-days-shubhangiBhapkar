872357bb7c9d433bf5ca622cc71a9ad48888d2ec
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-04-05 11:55:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	let isleap = false;
+	if(year % 4==0 && year % 100 !==0 || year % 400 == 0){
+		isleap = true;
+	}
+	return isleap ? 366: 365;
+}
+// let year = prompt("enter year");
+// alert(daysOfAYear(year));
